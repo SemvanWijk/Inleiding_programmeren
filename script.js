0a9ed027e58c84b43bd32d260f2f1edf6c4a6358
SemvanWijk <120381973+SemvanWijk@users.noreply.github.com>
2023-03-23 20:36:55 +0100

    change

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ var dy = 0;
 var jumping = false;
 
 var platformImg = new Image();
-platformImg.src = "./img/platform.png";
+platformImg.src = "inleiding_programmeren/img/platform.png";
 
 var signImg = new Image();
-signImg.src = "./img/sign.png";
+signImg.src = "inleiding_programmeren/img/sign.png";
 
 var coinImg = new Image();
-coinImg.src = "./img/coin.png";
+coinImg.src = "inleiding_programmeren/img/coin.png";
 
 var coin = {
 	x: 500,
@@ -70,7 +70,7 @@ var spriteY = 0;
 var frameCount = 0;
 
 var spriteSheet = new Image();
-spriteSheet.src = "./img/slimey.png";
+spriteSheet.src = "inleiding_programmeren/img/slimey.png";
 
 
 var coinLocations = [
